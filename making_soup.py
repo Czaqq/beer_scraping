291c589a3f859434080f707ae443cdab68ad9b7d
Czaqq <117601088+Czaqq@users.noreply.github.com>
2023-07-10 18:48:01 +0200

    Delete making_soup.py

--- making_soup.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def openLinkAndReturnSoup(root_link):
-    response = requests.get(root_link)
-    soup = BeautifulSoup(response.text, "html.parser")
-    return soup
