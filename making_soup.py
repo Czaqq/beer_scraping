ea45e78e6d4ec468387fbd5183d67a0b334795f9
Czaku <czaku17@gmail.com>
2023-07-10 19:32:15 +0200

    first commit

---
+++ making_soup.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+def openLinkAndReturnSoup(root_link):
+    response = requests.get(root_link)
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup
+
+# print(openLinkAndReturnSoup("https://ontap.pl"))
